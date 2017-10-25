f7219e8caa0c25b99fa8cca19154f94707136a15
Won Lee <won_lee@g.harvard.edu>
2017-10-25 07:17:57 -0400

    working b-r

--- simple_sfnn.py
+++ simple_sfnn.py
@@ -2,13 +2,14 @@
 Simple Stochastic Feedforward Neural Network
 W. Ryan Lee
 
-Implementation of a simple, 3-layer MLP using stochastic binary units.
+Implementation of a simple MLP using stochastic binary units.
 Uses data augmentation (latent h, weights W) for training/sampling.
 '''
 
 import numpy as np
 from scipy.special import expit
 from scipy.stats import norm
+from itertools import izip
 
 # Stochastic layer definition
 class BinaryLayer:
@@ -47,7 +48,7 @@ class OutputLayer:
 
 # Model definition
 class SimpleModel:
-    def __init__(self, input_dim = 200, hid_dim = 100, layers = 3, step_size = 0.01, sigma = 1):
+    def __init__(self, input_dim = 200, train_size = 100, hid_dim = 100, layers = 3, step_size = 0.01, sigma = 1):
         # Initialize hyperparams
         self.nlayer = layers # total layers = nlayer + 1 (output)
         self.input_dim = input_dim
@@ -72,35 +73,50 @@ class SimpleModel:
         ent = CrossEntropy()
         for l in range(self.nlayer - 1, -1, -1):
             # Backprop
-            prev = self.layers[l].det_forward(self.layers[l-1].h)
+            if l > 0:
+                prev = self.layers[l].det_forward(self.layers[l-1].h)
+                #print("Length: " + str(len(prev)))
+            else:
+                prev = self.layers[l].det_forward(x)
+                #print("The length is " + str(len(prev)))
             pred = self.layers[l+1].det_forward(prev)
             if l == self.nlayer - 1:
                 grad = mse.backward(pred, y) * self.layers[l+1].W # negative gradient
+                m = len(grad)
             else:
-                res = ent.backward(pred, true)
-                m = len(res)
-                grad = np.array([0]*m)
-                for k in range(m):
-                    grad = grad + res[k] * pred[k] * (1-pred[k]) * self.layers[l+1].W[:,k]
+                grad = np.array([0]*self.hid_dim)
+                i = 0
+                for p, q in izip(pred, self.layers[l+1].h):
+                    res = ent.backward(p, q)
+                    grad = grad + res * p * (1-p) * self.layers[l+1].W[:,i]
+                    i += 1
+                #res = ent.backward(pred, self.layers[l+1].h)
+                #m = len(res)
+                #grad = np.array([0]*m)
+                #for k in range(m):
+                #    grad = grad + res[k] * pred[k] * (1-pred[k]) * self.layers[l+1].W[:,k]
                 grad = (-1) * grad
-            prev = min(max(prev + self.step_size * grad, 0), 1)
+            prev = [min(max(t, 0.001), 0.999) for t in prev + self.step_size * grad]
             # Proposal
             prop = np.random.binomial(1, prev)
             # Accept-Reject
             curr = self.layers[l].h
-            next = self.layers[l+1].h
             proppred = self.layers[l+1].det_forward(prop)
             pred = self.layers[l+1].det_forward(curr)
             if l == self.nlayer - 1:
                 logalpha = np.log(norm(proppred, self.sigma).pdf(y) / norm(pred, self.sigma).pdf(y))
             else:
+                next = self.layers[l+1].h
                 logalpha = 0
                 for j in range(m):
                     logalpha = logalpha + next[j]*np.log(proppred[j]/pred[j]) + (1-next[j])*np.log((1-proppred[j])/(1-pred[j]))
             for j in range(m):
-                    logalpha = logalpha + (proppred[j] - pred[j])*np.log(prev[j]) + (pred[j] - proppred[j])*np.log(1-prev[j])
+                    logalpha = logalpha + (prop[j] - curr[j])*np.log(prev[j]) + (curr[j] - prop[j])*np.log(1-prev[j])
             if np.log(np.random.uniform()) < logalpha:
                 self.layers[l].h = prop
+
+    def logisticAndReject(X, y):
+        return True
             
             
         
@@ -130,24 +146,24 @@ X_train = np.load("data/X_train.npy")
 y_train = np.load("data/y_train.npy")
 
 # Model initialization
+train_size = len(X_train)
 epochs = 100
 layers = 3
 input_dim = 50
 hid_dim = 30
 
-model = SimpleModel(input_dim, hid_dim, layers) # Model init
+model = SimpleModel(input_dim, train_size, hid_dim, layers) # Model init
+
+# MCMC
 # Initial h sample
-pred = model.forward(X_train)
-y_loss = MSE()
-grad = y_loss.backward(pred, y_train)
-for l in range(layers, -1, -1):
-    # Backprop
-
-# Training
-#for t in range(epochs):
-    
-
-
+model.forward(X_train[0])
+# Gibbs
+for t in range(epochs):
+    print "Epoch: " + str(t)
+    for i in range(train_size):
+#        print("Obs: " + str(i))
+        model.backpropAndReject(X_train[i], y_train[i]) # h update for each obs
+    #model.logisticAndReject(X_train, y_train) # W update using all obs
 
 # Prediction
 
